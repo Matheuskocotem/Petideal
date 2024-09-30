72792a19b6f7c1eb0a4c0eae002adc8b2779f60b
Matheuskocotem <kocotemmatheus@gmail.com>
2024-09-30 11:46:13 -0300

    routes category e product ok

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -3,6 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\ProductController;
 
 /*
 |--------------------------------------------------------------------------
@@ -22,4 +24,23 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::get('/users', [AuthController::class, 'index']);
     Route::put('/users/{user}', [AuthController::class, 'update']);
     Route::delete('/users/{user}', [AuthController::class, 'destroy']);
+
+    // Rotas de categorias com prefixo 'api/categories'
+    Route::prefix('categories')->group(function () {
+        Route::get('/', [CategoryController::class, 'index']); 
+        Route::post('/', [CategoryController::class, 'store']); 
+        Route::get('/{category}', [CategoryController::class, 'show']); 
+        Route::put('/{category}', [CategoryController::class, 'update']); 
+        Route::delete('/{category}', [CategoryController::class, 'destroy']);
+    });
+
+    // Rotas de produtos com prefixo 'api/products'
+    Route::prefix('products')->group(function () {
+        Route::get('/', [ProductController::class, 'index']);
+        Route::get('/products/best-selling', [ProductController::class, 'bestSelling']);
+        Route::post('/', [ProductController::class, 'store']); 
+        Route::get('/{product}', [ProductController::class, 'show']); 
+        Route::put('/{product}', [ProductController::class, 'update']); 
+        Route::delete('/{product}', [ProductController::class, 'destroy']); 
+    });
 });
